6bec2ae610c891e5bfbc2607c5c525f661af628e
Felipe Araujo <felipe.rgla@gmail.com>
2024-09-17 09:09:06 -0300

    criado componentes do teste

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { useState } from "react";
+
 function App() {
+  const [atividade, setAtividade] = useState([])
+
+  function handleAdd () {
+    setAtividade()
+  }
+  console.log(atividade)
   return (
     <div className="App">
+      <form onSubmit={() => {handleAdd()}}>
+        <div>Insira a atividade:</div>
+        <input></input>
+        <br /><br />
+        <button type="submit" >Enviar</button>
+      </form>
+      
 
+      {/* <div>{atividade.splice(',')}</div> */}
     </div>
   );
 }
